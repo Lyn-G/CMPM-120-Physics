b9721f37d2f1401f62f4a8005e1b5aaac9576965
Lyn-G <132232498+Lyn-G@users.noreply.github.com>
2023-05-15 14:17:20 -0700

    main files uploaded

---
+++ README.md
@@ -0,0 +1,22 @@
+## Code requirements:<br>
+### The game uses both continuous and discrete inputs from the player:<br>
+When the player drags the ball and drags it in the air, there is continuous inputs. When the player releases the ball, the ball will either fall down because of gravity or fly through the air.
+
+### The player’s goal can only be achieved indirectly (by allowing the physics engine to move key objects into position/contact). <br>
+In order to make a basket, the player must have let go of the ball and the ball must go into the hole.
+
+### 3+ physics-based gameplay scenes (possibly implemented with a single Phaser Scene subclass).<br>
+1. Level1 (the first and easiest level in the game)
+2. Level2 (the second level in the game)
+3. Level3 (the last and hardest level in the game)
+
+### Other scenes are used to separate and contextualize the gameplay scenes<br>
+I have intro and outro scenes to signify that the game is starting or ending respectively. There are summary screens to show how long the player took to finish the level. <br><br>
+
+## Asset and code sources:<br> 
+I drew all my images with this [free online website](https://sketchpad.app/en/). All code is my own.
+1. Basketball.png
+2. star.png 
+3. arrow.png <br><br>
+
+## Link to play game.
---
+++ level3.js
@@ -0,0 +1,86 @@
+class Level3 extends Phaser.Scene{
+    constructor() {
+        super('Level3')
+    }
+    
+    preload() {
+        this.load.image('basketball', 'assets/Basketball.png');
+    }
+
+    create(data) {
+        this.matter.world.setBounds();  
+        this.startTime = data.end;
+        let total = 3;      
+        let hit1 = false;
+        let hit2 = false;
+        let hit3 = false;
+
+        let graphics = this.add.graphics();
+        graphics.fillStyle(0x000000);
+        let a = this.add.graphics().fillEllipse(1759, 550, 200, 100);
+        let b = this.add.graphics().fillEllipse(426, 200, 200, 100);
+        let c = this.add.graphics().fillEllipse(160, 1000, 200, 100);
+
+        let barrier = this.matter.add.rectangle(1759, 550, 200 , 100, {isSensor: true, isStatic: true});
+        barrier.gameObject = a;
+        // barrier.gameObject = barrier;
+
+        let barrier2 = this.matter.add.rectangle(426,200,200 ,100,  { isSensor: true, isStatic: true });
+        barrier2.gameObject = b;
+
+        let barrier3 = this.matter.add.rectangle(160,1000,200 ,100,  { isSensor: true, isStatic: true });
+        barrier3.gameObject = c;
+
+        // took this code from Phaser's examples
+        let circle = this.matter.add.image(1000, 900, 'basketball', null, { chamfer: 16 });
+        circle.setBody({
+        type: 'circle',
+        radius: 64
+        });
+        circle.setBounce(0.9);
+
+        this.matter.add.mouseSpring({ length: 1, stiffness: 0.6 });
+
+        // got this code from ChatGPT and modified some bits
+        // I gave it the prompt:
+        // "when the basketball and the ellipse barriers touch, go to another scene"
+        this.matter.world.on('collisionstart', (event) => {
+            let pairs = event.pairs;
+            for (let i = 0; i < pairs.length; i++) {
+                let pair = pairs[i];
+                if (pair.bodyA.gameObject === barrier.gameObject && pair.bodyB.gameObject === circle) {
+                    if (!this.input.activePointer.isDown && hit1 == false) {
+                        barrier.gameObject.destroy();
+                        total--;
+                        hit1 = true;
+                        
+                    }    
+                } 
+                if (pair.bodyA.gameObject === barrier2.gameObject && pair.bodyB.gameObject === circle) {
+                    if (!this.input.activePointer.isDown && hit2 == false) {
+                        barrier2.gameObject.destroy();
+                        total--;
+                        hit2 = true;
+                        
+                    }    
+                } 
+                if (pair.bodyA.gameObject === barrier3.gameObject && pair.bodyB.gameObject === circle) {
+                    if (!this.input.activePointer.isDown && hit3 == false) {
+                        barrier3.gameObject.destroy();
+                        total--;
+                        hit3 = true;
+                    }    
+                } 
+                
+            }
+            
+            if (total == 0) {
+                let end = this.time.now;
+                let elapsedTime = end - this.startTime;
+                this.scene.start('Summary', {elapsedTime, end});
+            }
+        });
+    }
+
+
+}
